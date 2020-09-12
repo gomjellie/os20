d975dd1863d79c15da68db574c923a37274542be
gomjellie <gomjellie@gmail.com>
2020-09-13 05:08:42 +0900

    fin ssu_shell

--- project2/ssu_shell.c
+++ project2/ssu_shell.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
@@ -7,6 +8,67 @@
 #define MAX_INPUT_SIZE 1024
 #define MAX_TOKEN_SIZE 64
 #define MAX_NUM_TOKENS 64
+
+char **tokenize(char *line);
+
+int main(int argc, char* argv[]) {
+    char  line[MAX_INPUT_SIZE];
+    char  **tokens;
+    int i;
+
+    FILE *fp;
+    pid_t pid;
+    int status;
+    if (argc == 2) {
+        fp = fopen(argv[1], "r");
+        if (fp < 0) {
+            printf("File doesn't exists.");
+            return -1;
+        }
+    }
+
+    while (1) {
+        /* BEGIN: TAKING INPUT */
+        bzero(line, sizeof(line));
+        if (argc == 2) { // batch mode
+            if (fgets(line, sizeof(line), fp) == NULL) { // file reading finished
+                break;
+            }
+            line[strlen(line) - 1] = '\0';
+        } else { // interactive mode
+            printf("$ ");
+            scanf("%[^\n]", line);
+            getchar();
+        }
+        // printf("Command entered: %s (remove this debug output later)\n", line);
+        /* END: TAKING INPUT */
+
+        line[strlen(line)] = '\n'; //terminate with new line
+        tokens = tokenize(line);
+        //do whatever you want with the commands, here we just print them
+
+        pid = fork();
+        if (pid < 0) {
+            fprintf(stderr,"fork error");
+        } else if (pid == 0) { // 자식 프로세스
+            execvp(tokens[0], tokens);
+        }
+
+        pid = waitpid(pid, &status, 0);
+
+        if (pid < 0) {
+            fprintf(stderr, "waitpid error");
+        }
+
+        // Freeing the allocated memory
+        for(i = 0; tokens[i] != NULL; i++) {
+            free(tokens[i]);
+        }
+        free(tokens);
+
+    }
+    return 0;
+}
 
 /**
  * Splits the string by space and returns the array of tokens
@@ -34,52 +96,4 @@ char **tokenize(char *line) {
     free(token);
     tokens[tokenNo] = NULL;
     return tokens;
-}
-
-int main(int argc, char* argv[]) {
-    char  line[MAX_INPUT_SIZE];
-    char  **tokens;
-    int i;
-
-    FILE *fp;
-    if (argc == 2) {
-        fp = fopen(argv[1], "r");
-        if (fp < 0) {
-            printf("File doesn't exists.");
-            return -1;
-        }
-    }
-
-    while (1) {
-        /* BEGIN: TAKING INPUT */
-        bzero(line, sizeof(line));
-        if (argc == 2) { // batch mode
-            if (fgets(line, sizeof(line), fp) == NULL) { // file reading finished
-                break;
-            }
-            line[strlen(line) - 1] = '\0';
-        } else { // interactive mode
-            printf("$ ");
-            scanf("%[^\n]", line);
-            getchar();
-        }
-        printf("Command entered: %s (remove this debug output later)\n", line);
-        /* END: TAKING INPUT */
-
-        line[strlen(line)] = '\n'; //terminate with new line
-        tokens = tokenize(line);
-        //do whatever you want with the commands, here we just print them
-
-        for (i = 0; tokens[i] != NULL; i++) {
-            printf("found token %s (remove this debug output later)\n", tokens[i]);
-        }
-
-        // Freeing the allocated memory
-        for(i = 0; tokens[i] != NULL; i++) {
-            free(tokens[i]);
-        }
-        free(tokens);
-
-    }
-    return 0;
 }
