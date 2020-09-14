ea63ac3198099cb7f677d3d566986dd22ea1938d
gomjellie <gomjellie@gmail.com>
2020-09-14 13:00:41 +0900

    stats 이중포인터 -> 포인터, timeout 3000으로 해야 3초됨;;

--- project2/ttop/main.c
+++ project2/ttop/main.c
@@ -4,12 +4,12 @@ int main(int argc, char const * argv[]) {
 	initscr();
 	noecho();
 	curs_set(false);
-	timeout(3);
+	timeout(3000);
 
 	while (true) {
 		stat_t *stats = malloc(sizeof(stat_t) * 300);
 		int stat_length;
-		stat_parse(&stats, &stat_length);
+		stat_parse(stats, &stat_length);
 		sort_proc(stats, stat_length - 1);
      	on_draw(stats, 15);
 		refresh();
--- project2/ttop/ttop.c
+++ project2/ttop/ttop.c
@@ -170,7 +170,7 @@ void read_stat(char *path, int position, stat_t stats[]) {
  * 이름의 숫자로 디렉토리를 확인합니다.
  * 찾은 디렉토리를 전달하는 read_print_stat 함수를 호출합니다.
  */
-void stat_parse(stat_t *stats[], int *stat_length) {
+void stat_parse(stat_t stats[], int *stat_length) {
      int i = 0;
      DIR *directory;
      struct dirent *dir;
@@ -191,7 +191,7 @@ void stat_parse(stat_t *stats[], int *stat_length) {
           strcat(current_path, directory_name_buffer);
           strcat(current_path, "/");
 
-          read_stat(current_path, i, *stats);
+          read_stat(current_path, i, stats);
           i ++;
      }
 
