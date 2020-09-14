1fdc640125d142fa2c500ca7d958b1e3844e71fe
gomjellie <gomjellie@gmail.com>
2020-09-14 23:31:33 +0900

    위아래 키보드로 이동

---
+++ project2/ttop/README.md
@@ -0,0 +1,6 @@
+
+
+# 참고자료
+
+https://wiki.kldp.org/wiki.php/NCURSES-Programming-HOWTO#s-1.4.4 - ncurses kldp자료
+
--- project2/ttop/ttop.c
+++ project2/ttop/ttop.c
@@ -7,21 +7,37 @@ typedef struct view {
     int scroll; // y 축 스크롤정도를 나타냄, 위 아래키를 통해서 조정 가능
 } view_t;
 
-void screen_init(view_t *this) {
+typedef enum direction {
+    UP, DOWN
+} direction_t;
+
+void view_init(view_t *this) {
     this->height = this->width = this->scroll = 0;
+}
+
+void view_scroll(view_t *this, direction_t to) {
+    if (to == DOWN) {
+        if (this->scroll < this->height)
+            this->scroll ++;
+    }
+    if (to == UP) {
+        if (0 < this->scroll)
+            this->scroll --;
+    }
 }
 
 void on_draw(const stat_t stats[], const int stats_len, const view_t ttop_view);
 
 int main(int argc, char const * argv[]) {
     initscr();
+    keypad(stdscr, TRUE);
     noecho();
     curs_set(false);
     timeout(3000);
 
     stat_t *stats = malloc(sizeof(stat_t) * 256);
     view_t ttop_view;
-    screen_init(&ttop_view);
+    view_init(&ttop_view);
 
     while (true) {
         int stats_length;
@@ -29,13 +45,25 @@ int main(int argc, char const * argv[]) {
         stats_update(stats, &stats_length);
 
         qsort(stats, stats_length, sizeof(stat_t), stat_cmp);
+        clear();
         on_draw(stats, stats_length, ttop_view);
         refresh();
         int ch = getch();
-        if (ch == 'q') break;
+        switch (ch) {
+            case 'q':
+            goto QUIT;
+            case KEY_UP:
+            view_scroll(&ttop_view, UP);
+            break;
+            case KEY_DOWN:
+            view_scroll(&ttop_view, DOWN);
+            break;
+        }
+        // if (ch == 'q') break;
 
     }
-    
+
+QUIT:
     endwin();
     free(stats);
 
@@ -55,17 +83,17 @@ void on_draw(const stat_t stats[], const int stats_len, const view_t ttop_view) 
     mvprintw(0, 30, "   TIME+\t");
     mvprintw(0, 40, "COMMAND");
 
-    for (int i = 0; i < stats_len; i++) {
+    for (int i = ttop_view.scroll; i < stats_len && i < ttop_view.height - 1; i++) {
         int hour = stats[i].time / 3600;
         int minute = (stats[i].time - (3600 * hour)) / 60;
         int second = (stats[i].time - (3600 * hour) - (minute * 60));
 
-        mvprintw(i + 1,  0, "%6d\t", stats[i].pid);
-        mvprintw(i + 1,  6, "%4ld\t", stats[i].priority);
-        mvprintw(i + 1, 10, "%3ld\t", stats[i].nice);
-        mvprintw(i + 1, 15, "%2c\t", stats[i].state);
-        mvprintw(i + 1, 20, "%6.2f\t", stats[i].cpu_usage);
-        mvprintw(i + 1, 30, "%02d:%02d:%02d\t", hour, minute, second);
-        mvprintw(i + 1, 40, "%s\t", stats[i].command);
+        mvprintw(i + 1 - ttop_view.scroll,  0, "%6d\t", stats[i].pid);
+        mvprintw(i + 1 - ttop_view.scroll,  6, "%4ld\t", stats[i].priority);
+        mvprintw(i + 1 - ttop_view.scroll, 10, "%3ld\t", stats[i].nice);
+        mvprintw(i + 1 - ttop_view.scroll, 15, "%2c\t", stats[i].state);
+        mvprintw(i + 1 - ttop_view.scroll, 20, "%6.2f\t", stats[i].cpu_usage);
+        mvprintw(i + 1 - ttop_view.scroll, 30, "%02d:%02d:%02d\t", hour, minute, second);
+        mvprintw(i + 1 - ttop_view.scroll, 40, "%s\t", stats[i].command);
     }
 }
