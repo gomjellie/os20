313fd433611a73e197252f60163ff18b2b844784
gomjellie <gomjellie@gmail.com>
2020-09-14 12:55:55 +0900

    3초마다 화면 갱신 + q 누르면 정상적으로 종료

--- project2/ttop/main.c
+++ project2/ttop/main.c
@@ -1,10 +1,10 @@
 #include "ttop.h"
 
 int main(int argc, char const * argv[]) {
-	// TODO: fork로 자식만들고 자식은 계속 stats 3초마다 갱신하게, 부모는 drawing 계속하게...
 	initscr();
 	noecho();
 	curs_set(false);
+	timeout(3);
 
 	while (true) {
 		stat_t *stats = malloc(sizeof(stat_t) * 300);
@@ -14,8 +14,8 @@ int main(int argc, char const * argv[]) {
      	on_draw(stats, 15);
 		refresh();
 		int ch = getch();
-		if (ch == 'q') return 0;
-		sleep(3);
+		if (ch == 'q') break;
+		// sleep(3);
 		free(stats);
 	}
 	
