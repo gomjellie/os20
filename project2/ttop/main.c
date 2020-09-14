fdea9fdff436e5455f42b1ae10f2ad4632b028f2
gomjellie <gomjellie@gmail.com>
2020-09-14 12:45:04 +0900

    main에 실행흐름 들어가게 변경

--- project2/ttop/main.c
+++ project2/ttop/main.c
@@ -1,20 +1,24 @@
 #include "ttop.h"
 
 int main(int argc, char const * argv[]) {
+	// TODO: fork로 자식만들고 자식은 계속 stats 3초마다 갱신하게, 부모는 drawing 계속하게...
 	initscr();
+	noecho();
+	curs_set(false);
 
-	while(true) {
+	while (true) {
 		stat_t *stats = malloc(sizeof(stat_t) * 300);
 		int stat_length;
 		stat_parse(&stats, &stat_length);
 		sort_proc(stats, stat_length - 1);
      	on_draw(stats, 15);
 		refresh();
+		int ch = getch();
+		if (ch == 'q') return 0;
 		sleep(3);
 		free(stats);
 	}
 	
-	getch();
 	endwin();
 
 	return 0;
