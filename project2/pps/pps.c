83358a25fa5049d7325609af7028bc32803baad1
gomjellie <gomjellie@naver.com>
2020-09-20 04:11:46 +0900

    pps 옵션없이 동작

--- project2/pps/pps.c
+++ project2/pps/pps.c
@@ -1,40 +1,35 @@
 #include <ncurses.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 #include "stat.h"
 
-// typedef struct state {
-//     int pid;
-//     int idx;
-//     char stat;
-// } state_t;
-
-// int pid2idx[24000];
+#define DEFAULT_FORMAT "%5s %s\t%8s %s\n"
 
 int main(int argc, char *argv[]) {
     if (argc > 1) {
         // 옵션 인자가 들어온경우 처리
     } 
 
-    printf("Your tty name is : %s\n", ttyname(0)); return 0;
     stat_t stats[256];
     int stats_length;
     stats_update(stats, &stats_length);
 
-    // for (int i = 0; i < stats_length; i++) {
-    //     int pid = stats[i].pid;
-    //     pid2idx[pid] = i;
-    // }
+    int fd = open("/proc/self/fd/0", O_RDONLY);
+    char self_tty[32], time_s[32];
+    sprintf(self_tty, "%s", ttyname(fd));
 
+    printf("  PID TTY          TIME CMD\n");
     for (int i = 0; i < stats_length; i++) {
         stat_t stat = stats[i];
-        if (stat.state == 'R') {
+        if (strcmp(stat.tty, self_tty) == 0) {
             int hour = stat.time / 3600;
             int minute = (stat.time - (3600 * hour)) / 60;
             int second = (stat.time - (3600 * hour) - (minute * 60));
             
-            // printf(" %5d  %d    %c   %3d:%02d:%02d %s\n", stat.pid, stat.tty_nr, stat.state, hour, minute, second, stat.command);
+            sprintf(time_s, "%02d:%02d:%02d", hour, minute, second);
+            printf("%5d %s    %8s %s\n", stat.pid, stat.tty + 5, time_s, stat.command);
         }
     }
 }
