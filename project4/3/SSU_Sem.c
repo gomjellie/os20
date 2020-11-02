487fabff2c08d96ff236b48d0abd4786fddc7b90
gomjellie <gomjellie@naver.com>
2020-11-03 05:55:39 +0900

    project4 - 3 디버깅용 프린트 제거

--- project4/3/SSU_Sem.c
+++ project4/3/SSU_Sem.c
@@ -8,10 +8,6 @@
 #include <unistd.h>
 #include "SSU_Sem.h"
 
-#ifndef PRINT_FUNC_CALL
-// #define PRINT_FUNC_CALL
-#endif
-
 void SSU_Sem_init(SSU_Sem *s, int value) {
   *s = (SSU_Sem) {
     .counter = value,
@@ -24,39 +20,25 @@ void SSU_Sem_init(SSU_Sem *s, int value) {
 
 void SSU_Sem_down(SSU_Sem *s) {
   pthread_mutex_lock(&s->mutex_down);
-#ifdef PRINT_FUNC_CALL
-  printf("SSU_Sem_down entered id: %d\n", (int)pthread_self());
-#endif
   pthread_mutex_lock(&s->mutex_counter);
 
   s->counter --;
   usleep(500);
   while (s->counter < 0) {
-    // printf("counter: %d\n", s->counter);
     pthread_cond_wait(&s->signal, &s->mutex_counter);
   }
   
-  pthread_mutex_unlock(&s->mutex_counter);
-  
-#ifdef PRINT_FUNC_CALL
-  printf("SSU_Sem_down exit id: %d\n", (int)pthread_self());
-#endif
+  pthread_mutex_unlock(&s->mutex_counter);  
   pthread_mutex_unlock(&s->mutex_down);
 }
 
 void SSU_Sem_up(SSU_Sem *s) {
   pthread_mutex_lock(&s->mutex_up);
-#ifdef PRINT_FUNC_CALL
-  printf("SSU_Sem_up entered id: %d\n", (int)pthread_self());
-#endif
   pthread_mutex_lock(&s->mutex_counter);
   s->counter ++;
   usleep(500);
 
   pthread_cond_broadcast(&s->signal);
   pthread_mutex_unlock(&s->mutex_counter);
-#ifdef PRINT_FUNC_CALL
-  printf("SSU_Sem_up exit %d\n", (int)pthread_self());
-#endif
   pthread_mutex_unlock(&s->mutex_up);
 }
